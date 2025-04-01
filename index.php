5c75cd88001620c1016774e1d56eacdd0ab0fd7b
Kaj van Es <9024897@student.zadkine.nl>
2025-04-01 16:07:59 +0200

    v0.3.0
    Beginnende home pagina (index.php)
    + Navigatiebalk (nav.php)

---
+++ index.php
@@ -0,0 +1,6 @@
+    <div class="background2">
+        <?php require "nav.php";?>
+        <?PHP session_start(); echo $_SESSION['username']?>
+    </div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Chirpify</title>
+</head>
+<body>
+    <nav>
+        <a href="index.php">
+            <img src="img/logo.png" alt="">
+        </a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+        </ul>
+    </nav>
